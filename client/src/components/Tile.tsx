081e546bc335cdb455e6c25ac4fa1f07fcde8866
Dominik Tarnowski <dominik.t.uk@gmail.com>
2019-06-02 13:04:51 +0100

    Allow toggling Tile component content visilibity

--- client/src/components/Tile.tsx
+++ client/src/components/Tile.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useBoolean } from "@hook-state/core";
 
 interface TileProps {
   title: string;
@@ -6,10 +7,13 @@ interface TileProps {
 }
 
 export default function Tile({ title, children }: TileProps) {
+  const { value, toggle } = useBoolean(true);
   return (
     <div className="tile">
-      <div className="tile-title">{title}</div>
-      <div className="tile-content">{children}</div>
+      <div className="tile-title" onClick={toggle}>
+        {title}
+      </div>
+      {value && <div className="tile-content">{children}</div>}
     </div>
   );
 }

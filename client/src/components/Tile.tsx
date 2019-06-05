82caac5468aaffa92a26d3898387f9c28f503ba0
Dominik Tarnowski <dominik.t.uk@gmail.com>
2019-06-05 20:26:16 +0100

    Fixes #3

--- client/src/components/Tile.tsx
+++ client/src/components/Tile.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useBoolean } from "@hook-state/core";
+import ArrowIcon from "@material-ui/icons/ArrowDropDown";
 
 interface TileProps {
   title: string;
@@ -7,11 +8,12 @@ interface TileProps {
 }
 
 export default function Tile({ title, children }: TileProps) {
-  const { value, toggle } = useBoolean(true);
+  const { value, toggle } = useBoolean(true, { persist: title });
   return (
     <div className="tile">
-      <div className="tile-title" onClick={toggle}>
-        {title}
+      <div className="tile-titlenav" onClick={toggle}>
+        <ArrowIcon className={`tile-icon ${value ? "-closed" : ""}`} />
+        <div className="tile-title">{title}</div>
       </div>
       {value && <div className="tile-content">{children}</div>}
     </div>

4b6b67608855be148ab3547c2533dc5d6d5bb78d
Dominik Tarnowski <dominik.t.uk@gmail.com>
2019-06-06 11:05:30 +0100

    Distinguish between stdout and stderr

--- client/src/components/tiles/TerminalOutput.tsx
+++ client/src/components/tiles/TerminalOutput.tsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import Stdout from "containers/Stdout";
 
 export default function TerminalOutput() {
   const stdout = Stdout.use();
+  const bottom = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (bottom.current) {
+      bottom.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [stdout.value]);
+
   return (
     <div className="full scrollable">
       {stdout.value.map((cmd, i) => (
         <div key={i}>
           <div>> {cmd.command}</div>
-          {cmd.output.map((line, j) => (
-            <div className="text-darker" key={j}>
-              {line}
-            </div>
-          ))}
+          <div className={`text-${cmd.success ? "darker" : "error"}`}>
+            {cmd.output.map((line, j) => (
+              <div key={j}>{line}</div>
+            ))}
+          </div>
+          <div ref={bottom} />
         </div>
       ))}
     </div>

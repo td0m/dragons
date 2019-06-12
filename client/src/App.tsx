b3046ef23e19b6d40fda40337bd9eb7af1f889e2
Dominik Tarnowski <dominik.t.uk@gmail.com>
2019-06-12 23:59:48 +0100

    fix layout issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
   const connected = api.connectionState === ConnectionState.TargetConnected;
 
   return (
-    <div className="w-screen h-screen bg-clay-500 text-clay-100 font-mono overflow-auto">
+    <div className="w-full min-h-screen h-full bg-clay-500 text-clay-100 font-mono relative flex flex-col">
       <div
         className="flex items-center pl-4 pr-4 border-b border-clay-400"
         style={{ height: 56 }}
@@ -33,11 +33,8 @@ const App: React.FC = () => {
         <div className="flex-1" />
         {connected && <TopActions />}
       </div>
-      <div
-        className="flex flex-col md:flex-row"
-        style={{ height: "calc(100% - 56px)" }}
-      >
-        <div className="w-full md:w-64 xs:border-b md:border-r border-clay-400 p-2 md:overflow-auto">
+      <div className="flex flex-col md:flex-row flex-1">
+        <div className="w-full md:h-full md:w-64 xs:border-b md:border-r border-clay-400 p-2">
           <Clock />
           <Divider />
           <NetworkStatus />
